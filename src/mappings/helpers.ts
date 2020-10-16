3a421f0e309e8a130fd914efff9ecccd850fb2e9
Ubuntu <ubuntu@ip-172-31-48-42.ec2.internal>
2020-10-16 18:07:33 +0000

    update repository

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -7,7 +7,7 @@ import { User, Bundle, Token, LiquidityPosition, LiquidityPositionSnapshot, Pair
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x7d7847A9F604314d64E8B48B09440d5bff6f6314'
+export const FACTORY_ADDRESS = '0x7d7847a9f604314d64e8b48b09440d5bff6f6314'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
